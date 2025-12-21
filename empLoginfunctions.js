bdd4dee3d37739e10a380a4faa9f5bdf387ad1eb
hitesh0806 <hiteshtg812@gmail.com>
2025-12-21 17:04:15 +0530

    Created login function of employee

---
+++ empLoginfunctions.js
@@ -0,0 +1,27 @@
+let dataArray = JSON.parse(localStorage.getItem('dataArray'));
+let name_ = document.querySelector('.js-user-name');
+let password_ = document.querySelector('.js-user-password');
+
+document.querySelector('.js-signInBtn').
+addEventListener('click', () => {
+login();
+})
+
+function login(){
+    let found = false;
+    const Name = name_.value;
+    const Password = password_.value;
+    if(dataArray){
+        dataArray.forEach((element) => {
+            if(Name === element.personalData.Name && 
+                Password === element.personalData.Password){
+                console.log('user match found');
+                window.location.href = 'empHomePage.html';
+                found = true;
+            }
+        });
+        if(!found){
+            console.log('User not found.')
+        }
+    }
+}
